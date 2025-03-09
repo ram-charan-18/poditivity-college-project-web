1a481d2824d7a02e917f693390a7a11bbda74ada
chandhu4796 <rc1250823@gmail.com>
2025-03-09 14:04:38 +0530

    updated my html code and javascript code
    updated my html code and javascript code

---
+++ code /javascript.js
@@ -0,0 +1,22 @@
+let a = 15
+let b =1
+
+console.log(a +b);
+
+if (a +b){
+    console.log( 14);
+}else{
+console.log( "wrong answer" );
+}
+
+
+let num=15
+if (num%3==0 && num%5==0){
+console.log("Divisible by 3 and 5 ");
+}else if (num%3==0 && num%5==0){
+console.log("Divisible by 5")    
+}else if (num%3==0){
+console.log("Divisible by 3")    
+}else{
+    
+}
